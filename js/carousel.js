9e1aa917f1ee80be6e5bf2018da1e92a36212045
Diana Alcala <diana.alcalasandoval@gmail.com>
2024-06-08 00:11:52 -0600

    Sliders

---
+++ js/carousel.js
@@ -0,0 +1,16 @@
+$('.trend-carousel, .you-carousel,.movies-carousel, .series-carousel').flickity({
+    "autoPlay": true,
+    "freeScroll": true,
+    "contain": true,
+    "prevNextButtons": false,
+    "pageDots": false,
+    "wrapAround": true
+});
+
+$('.watching-carousel').flickity({
+    "cellAlign": "left",
+    "contain": true,
+    "prevNextButtons": false,
+    "pageDots": false
+});
+
